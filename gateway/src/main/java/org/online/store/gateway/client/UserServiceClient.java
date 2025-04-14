8e9ada9243cf8777370e583e790a4a9f036505af
parviz <parvizumarov78@gmail.com>
2025-04-14 11:35:36 +0300

    update readme file and method of connecting to another microservice

--- gateway/src/main/java/org/online/store/gateway/client/UserServiceClient.java
+++ gateway/src/main/java/org/online/store/gateway/client/UserServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import reactor.core.publisher.Mono;
 
-@FeignClient(name = "userService", url = "${reactive.feign.client.config.userService.url}")
+@FeignClient(name = "userservice")
 public interface UserServiceClient {
     @GetMapping("/api/user")
     SimpleResponse getUser();
